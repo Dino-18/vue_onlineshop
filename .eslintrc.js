d5ca826e2d8a02ce9e49c682575e29c788956a33
Dino-18 <490783980@qq.com>
2021-01-29 12:25:17 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,12 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    // '@vue/standard'
+    '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    'space-before-function-paren': 0,
-    'no-irregular-whitespace': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from 'C:/Users/admin/Desktop/code/vue_shop/src/components/Login.vue'
-import Home from 'C:/Users/admin/Desktop/code/vue_shop/src/components/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
